0201296c38c1b80fdf158f326bb98abb7a66329a
root <root@DESKTOP-VM5BUBB>
2024-04-23 10:28:43 +0200

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"webfejl-pe-2024","appId":"1:546988117878:web:97ed92e63556e486c1b457","storageBucket":"webfejl-pe-2024.appspot.com","apiKey":"AIzaSyD30x2Xx0NY_IS0ObtZTfEOvaKVBBvFKy8","authDomain":"webfejl-pe-2024.firebaseapp.com","messagingSenderId":"546988117878"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
